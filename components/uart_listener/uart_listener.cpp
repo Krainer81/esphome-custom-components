3de0d5462d93df8cc8d5bf1e6a9daee4de30a0ea
Krainer81 <65104583+Krainer81@users.noreply.github.com>
2025-08-10 22:16:42 +0200

    Add files via upload

---
+++ components/uart_listener/uart_listener.cpp
@@ -0,0 +1,7 @@
+#include "uart_listener.h"
+
+namespace esphome {
+namespace uart_listener {
+// Nic speciálního zde není potřeba, vše je v .h
+}  // namespace uart_listener
+}  // namespace esphome
