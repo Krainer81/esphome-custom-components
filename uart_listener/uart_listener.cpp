e2f98593c0a0d4ce4bb56ab0c6e285602139dc64
Krainer81 <65104583+Krainer81@users.noreply.github.com>
2025-08-10 22:14:55 +0200

    Delete uart_listener/uart_listener.cpp

--- uart_listener/uart_listener.cpp
+++
@@ -1,7 +0,0 @@
-#include "uart_listener.h"
-
-namespace esphome {
-namespace uart_listener {
-// Nic speciálního zde není potřeba, vše je v .h
-}  // namespace uart_listener
-}  // namespace esphome
